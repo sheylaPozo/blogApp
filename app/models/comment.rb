3e8b43efb896906aba8344cca85f9711ee6b945a
sheylaPozo <shiggipoke@gmail.com>
2021-11-10 05:41:50 -0500

    counter recent comments method to post model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_create :update_counter
+  after_create :update_count
 
   def update_counter
     post.update(comments_counter: post.comments.size)
