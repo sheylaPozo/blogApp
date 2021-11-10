ef056c2acad7ba88f7ca9218231b979ddd56a1e0
sheylaPozo <shiggipoke@gmail.com>
2021-11-10 05:43:32 -0500

    Add recent_comments method to Post model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_create :update_count
+  after_create :update_counter
 
   def update_counter
     post.update(comments_counter: post.comments.size)
