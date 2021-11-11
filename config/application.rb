3678c2154da7c251d6781e7cd9ae6e357b63b76e
sheylaPozo <shiggipoke@gmail.com>
2021-11-11 17:08:01 -0500

    add flash message on create

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listed in Gemfile, including any gem
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
