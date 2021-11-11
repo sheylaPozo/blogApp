678690f9d2836898e395dc1040fdcaf706aff735
sheylaPozo <shiggipoke@gmail.com>
2021-11-11 09:06:33 -0500

    refactor comments/controller

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,5 +3,5 @@ class ApplicationJob < ActiveJob::Base
   # retry_on ActiveRecord::Deadlocked
 
   # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationErro
+  # discard_on ActiveJob::DeserializationError
 end
